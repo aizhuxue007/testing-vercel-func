8a897917cc72043aac304919a5b3952b2ec870eb
Aizhu Xue <aizhudeveloper@gmail.com>
2025-07-01 15:26:02 -0400

    return json

--- src/app/api/hello.js
+++ src/app/api/hello.js
@@ -1,5 +1,20 @@
 export async function GET(request) {
-  const response = await fetch("https://api.vercel.app/products");
-  const products = await response.json();
-  return Response.json(products);
+  const rappers = {
+    "21 savage": {
+      birthName: "Shéyaa Bin Abraham-Joseph",
+      birthPlace: "Plaistow, London, England",
+      age: 32,
+    },
+    "chance the rapper": {
+      birthName: "Chancelor Bennett",
+      birthPlace: "Chicago, Illinois",
+      age: 32,
+    },
+    dylan: {
+      birthName: "dylan",
+      birthPlace: "dylan",
+      age: 29,
+    },
+  };
+  return Response.json(rappers);
 }
